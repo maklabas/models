212b98a845fead63ab7ba1941ca45e12f26e3786
Pahan <5972140@gmail.com>
2021-07-08 17:01:52 +0300

    закончено

--- courses/models.py
+++ courses/models.py
@@ -1,5 +1,15 @@
+from datetime import date
 from django.db import models
+from teachers.models import Teacher
+
 
 # Create your models here.
 class Course(models.Model):
-    name =
+    name = models.CharField(max_length=64, blank=False)
+    description = models.TextField()
+    teacher = models.ForeignKey(Teacher, on_delete=models.CASCADE)
+    start = models.DateField(blank=True, default=date.today)
+    end = models.DateField(blank=True, default=date.today)
+
+    def __str__(self):
+        return self.name
--- models/settings.py
+++ models/settings.py
@@ -38,6 +38,8 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'teachers.apps.TeachersConfig',
+    'courses.apps.CoursesConfig',
+    'students.apps.StudentsConfig',
 ]
 
 MIDDLEWARE = [
